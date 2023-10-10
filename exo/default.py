c72dd83d8db24bae53886f280ac0c7585d227bc4
MartinVerot <laterite@laposte.net>
2023-10-10 13:33:24 +0200

    default.py

---
+++ exo/default.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Descriptif du fichier
+"""
+
+# Importation des librairies
+
+# Definition des fonctions
+
+# Programme principal
+if __name__ == "__main__":
+   pass
+
